e852f4dcde036f301587127a278ce631c9001b8a
Anish9039 <anishthapa9039@gmail.com>
2024-08-11 18:38:22 +0545

    portfolio section

---
+++ src/components/Portfolio.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div >
+        <div>
+
+
+            <p>Portfolio</p>
+
+            <p>Check out my work right here </p>  
+
+        </div>
+
+        <div>
+            <div></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Portfolio
+
