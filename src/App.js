8966acc2686812bb6aae33a24e1cb8336f861d4a
Anish9039 <anishthapa9039@gmail.com>
2024-08-14 15:38:24 +0545

    Added images on portfolio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/navbar';
 import Home from './components/home'
 import SocialLinks from './components/SocialLinks';
 import AboutUs from './components/About';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
   <Home />
   <SocialLinks />
   <AboutUs />
+  <Portfolio />
   </div>
     
   );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,21 +1,68 @@
-import React from 'react'
+import React from 'react';
+import er from '../assets/portfolio/er.jpeg';
+import qw from '../assets/portfolio/qw.jpeg';
+import sd from '../assets/portfolio/sd.jpg';
+import ty from '../assets/portfolio/ty.jpeg';
+
 
 const Portfolio = () => {
+ 
+
+  const portfolio = [
+    {
+      id:1,
+      src:er 
+    },
+    {
+      id:2,
+      src:qw
+    },
+
+    {
+      id:3,
+      src:sd
+    },
+    {
+      id:4,
+      src:ty
+    }
+
+
+
+  ]
+
   return (
-    <div >
-        <div>
+    <div name="portfolio" className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen' >
+        <div className='max-w-screen-lg p-6 mx-auto flex flex-col justify-center w-full h-full'>
 
+      <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500' >Portfolio</p>
 
-            <p>Portfolio</p>
+            <p className='py-6'>Check out my work right here </p>  
 
-            <p>Check out my work right here </p>  
+      </div>
 
+      <div  className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0' >
+
+      {
+      portfolio.map(({id,src}) =>(
+        <div key={id} className='shadow-md shadow-gray-600 rounded-lg' >
+          
+          <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
+          <div className='flex items-center justify-center'>
+            <button className='w-12 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >Code</button>
+
+          </div>
+            
         </div>
 
-        <div>
-            <div></div>
-        </div>
+      ))
       
+      }
+
+</div>
+        </div>
     </div>
   )
 }
